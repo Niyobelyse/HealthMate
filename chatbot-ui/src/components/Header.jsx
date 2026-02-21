82e1119bde68103cb25a99ad19474f29acf21fd5
Niyobelyse <ganzabelyse@gmail.com>
2026-02-21 20:07:29 +0200

    clear chat removed

--- chatbot-ui/src/components/Header.jsx
+++ chatbot-ui/src/components/Header.jsx
@@ -23,12 +23,12 @@ function Header({ modelType, setModelType, onClear }) {
             </select>
           </div>
 
-          <button
+          {/* <button
             onClick={onClear}
             className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors"
           >
             Clear Chat
-          </button>
+          </button> */}
         </div>
       </div>
     </header>
